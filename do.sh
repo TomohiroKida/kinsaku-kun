a29bb6931765b476a61b214b0bc771725a990352
Tomohiro <tomo426.sakura1281m@gmail.com>
2022-12-25 16:36:49 +0900

    Update do.sh
    fix venv command in do.sh

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mac_addr=XX:XX:XX:XX:XX:XX
-source venv/bin/active
+source venv/bin/activate
 sudo hcitool -i hci0 cmd 0x04 0x009
 sudo hcitool -i hci0 cmd 0x3f 0x001 0x56 0x34 0x12 0xCB 0x58 0x94
 sudo systemctl restart bluetooth.service
