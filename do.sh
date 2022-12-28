cc10eba7073588dd732635527595d29c5c86a73e
TomohiroKida <git@github.com:TomohiroKida/kinsaku-kun.git>
2022-12-28 10:28:08 +0000

    ?

--- do.sh
+++ do.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mac_addr=XX:XX:XX:XX:XX:XX
-source venv/bin/active
+source venv/bin/activate
 sudo hcitool -i hci0 cmd 0x04 0x009
 sudo hcitool -i hci0 cmd 0x3f 0x001 0x56 0x34 0x12 0xCB 0x58 0x94
 sudo systemctl restart bluetooth.service
-sudo ~/kinsaku-kun/venv/bin/joycontrol-pluginloader -r ${mac_addr} ./ContinueButtonA.py
+sudo ~/work/kinsaku-kun/venv/bin/joycontrol-pluginloader -r ${mac_addr} ./ContinueButtonA.py
